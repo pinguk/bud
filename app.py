a6cad66f5b48a22b0477b51e1907dd89f38cf092
mr_pingu <ingu.kang95@gmail.com>
2021-01-03 21:33:51 +0900

    Commit #3

--- app.py
+++ app.py
@@ -1,16 +1,16 @@
-# This is a sample Python script.
+from pymongo import MongoClient
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from flask import Flask, render_template, jsonify, request
 
+app = Flask(__name__)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+client = MongoClient('localhost', 27017)
+db = client.dbJustGo
 
+# HTML 화면 보여주기
+@app.route('/')
+def home():
+    return render_template('index.html')
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    app.run('0.0.0.0', port=5009, debug=True)
