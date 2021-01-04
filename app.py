abd34aac3e7b2a788743228b89b155477f228b9f
mr_pingu <ingu.kang95@gmail.com>
2021-01-04 15:11:32 +0900

    Commit #3

--- app.py
+++ app.py
@@ -12,5 +12,13 @@ db = client.dbJustGo
 def home():
     return render_template('index.html')
 
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup_page():
+    return render_template('signup.html')
+
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5009, debug=True)
