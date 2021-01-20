d4809578b5401c7bbd8d3e02c4f914da4da5ae78
mr_pingu <ingu.kang95@gmail.com>
2021-01-21 02:10:04 +0900

    hot fix

--- app.py
+++ app.py
@@ -93,4 +93,4 @@ def recall_loans():
     return jsonify({'result': 'success', 'loans': loans})
 
 if __name__ == '__main__':
-    app.run('0.0.0.0', port=5000, debug=True)
+    app.run('0.0.0.0', port=5009, debug=True)
